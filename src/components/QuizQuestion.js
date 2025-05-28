98d6be13c866ad8e191cc870ba5201fd0b6e3363
ranjanhivoco <ranjan@hivoco.com>
2025-05-28 12:30:53 +0530

    quiz text center

--- src/components/QuizQuestion.js
+++ src/components/QuizQuestion.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 const QuizQuestion = ({question,className,questionIndex}) => {
   return (
     <div className="relative w-full flex justify-center items-center">
-    <h3 className="font-normal text-[22px] leading-6 z-50 absolute py-8 px-7 text-center ">
+    <h3 className="font-normal text-[22px] leading-6 z-50 absolute py-8 px-7 text-center mx-auto ">
        <span>Q{questionIndex+1}:</span> {question}
     </h3>
 
