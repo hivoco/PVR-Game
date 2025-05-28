9ca73efcd882cc5ffd61f73f7bdf84155bcdf17b
ranjanhivoco <ranjan@hivoco.com>
2025-05-28 13:21:56 +0530

    global music added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,22 @@
+import { MusicProvider } from "@/context/MusicContext";
 import "@/styles/globals.css";
 import { Bebas_Neue } from "next/font/google";
 
-
 const bebasNeue = Bebas_Neue({ subsets: ["latin"], weight: ["400"] });
 export default function App({ Component, pageProps }) {
+
+  const pagesToPlayOn = [
+    "/",
+    "/info",
+    "/result",
+  ];
+
+
   return (
     <main className={bebasNeue.className}>
-      <Component {...pageProps} />
+      <MusicProvider musicUrl="/music/global.wav" pagesToPlayOn={pagesToPlayOn}>
+        <Component {...pageProps} />
+      </MusicProvider>
     </main>
   );
 }
